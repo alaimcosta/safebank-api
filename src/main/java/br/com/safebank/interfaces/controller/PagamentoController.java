31524624a7d9b1e567251193240b3af02c018c51
Alaim de Jesus Leão Costa <alaim.costa@qintess.com>
2026-03-23 21:42:57 -0300

    ajustando documentação

--- src/main/java/br/com/safebank/interfaces/controller/PagamentoController.java
+++ src/main/java/br/com/safebank/interfaces/controller/PagamentoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("pagamento")
+@RequestMapping("v1/pagamento")
 class PagamentoController {
 
     @Autowired
